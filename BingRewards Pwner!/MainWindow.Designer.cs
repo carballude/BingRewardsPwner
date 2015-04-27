d313defd8c2f0e1bdc838d37cd1b3c0dcd7fc2c4
Pablo Carballude <carballude@live.com>
2015-04-27 00:12:31 -0700

    Initial release
    This is the initial release... and likely the last one since this was
    quickly done just to amuse myself on a sunday afternoon.

---
+++ BingRewards Pwner!/MainWindow.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace BingRewardsPwner
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.minimumSecondsUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maximumSecondsUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.desktopSearchesNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.mobileSearchesNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.startButton = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minimumSecondsUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maximumSecondsUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.desktopSearchesNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mobileSearchesNumericUpDown)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(206, 492);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.flowLayoutPanel1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(206, 456);
+            this.panel4.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.groupBox1);
+            this.flowLayoutPanel1.Controls.Add(this.groupBox2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(206, 456);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User data";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.flowLayoutPanel2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 16);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(194, 81);
+            this.panel2.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.textBox1);
+            this.flowLayoutPanel2.Controls.Add(this.label2);
+            this.flowLayoutPanel2.Controls.Add(this.textBox2);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(194, 81);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(188, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.panel5);
+            this.groupBox2.Location = new System.Drawing.Point(3, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 184);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.flowLayoutPanel3);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(3, 16);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(194, 165);
+            this.panel5.TabIndex = 0;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.label3);
+            this.flowLayoutPanel3.Controls.Add(this.minimumSecondsUpDown);
+            this.flowLayoutPanel3.Controls.Add(this.label4);
+            this.flowLayoutPanel3.Controls.Add(this.maximumSecondsUpDown);
+            this.flowLayoutPanel3.Controls.Add(this.label5);
+            this.flowLayoutPanel3.Controls.Add(this.desktopSearchesNumericUpDown);
+            this.flowLayoutPanel3.Controls.Add(this.label6);
+            this.flowLayoutPanel3.Controls.Add(this.mobileSearchesNumericUpDown);
+            this.flowLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel3.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(194, 165);
+            this.flowLayoutPanel3.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Minum seconds between requests:";
+            // 
+            // minimumSecondsUpDown
+            // 
+            this.minimumSecondsUpDown.Location = new System.Drawing.Point(3, 16);
+            this.minimumSecondsUpDown.Name = "minimumSecondsUpDown";
+            this.minimumSecondsUpDown.Size = new System.Drawing.Size(188, 20);
+            this.minimumSecondsUpDown.TabIndex = 1;
+            this.minimumSecondsUpDown.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Maximum seconds between requests:";
+            // 
+            // maximumSecondsUpDown
+            // 
+            this.maximumSecondsUpDown.Location = new System.Drawing.Point(3, 55);
+            this.maximumSecondsUpDown.Name = "maximumSecondsUpDown";
+            this.maximumSecondsUpDown.Size = new System.Drawing.Size(188, 20);
+            this.maximumSecondsUpDown.TabIndex = 3;
+            this.maximumSecondsUpDown.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Number of Desktop searches:";
+            // 
+            // desktopSearchesNumericUpDown
+            // 
+            this.desktopSearchesNumericUpDown.Enabled = false;
+            this.desktopSearchesNumericUpDown.Location = new System.Drawing.Point(3, 94);
+            this.desktopSearchesNumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.desktopSearchesNumericUpDown.Name = "desktopSearchesNumericUpDown";
+            this.desktopSearchesNumericUpDown.Size = new System.Drawing.Size(188, 20);
+            this.desktopSearchesNumericUpDown.TabIndex = 5;
+            this.desktopSearchesNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(139, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Number of Mobile searches:";
+            // 
+            // mobileSearchesNumericUpDown
+            // 
+            this.mobileSearchesNumericUpDown.Enabled = false;
+            this.mobileSearchesNumericUpDown.Location = new System.Drawing.Point(3, 133);
+            this.mobileSearchesNumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.mobileSearchesNumericUpDown.Name = "mobileSearchesNumericUpDown";
+            this.mobileSearchesNumericUpDown.Size = new System.Drawing.Size(188, 20);
+            this.mobileSearchesNumericUpDown.TabIndex = 7;
+            this.mobileSearchesNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.startButton);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 456);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(206, 36);
+            this.panel3.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(9, 6);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(188, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start!";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.webBrowser1);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(206, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(738, 492);
+            this.panel6.TabIndex = 1;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(738, 492);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 492);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainWindow";
+            this.Text = "Bing Rewards Pwned!";
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minimumSecondsUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maximumSecondsUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.desktopSearchesNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mobileSearchesNumericUpDown)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown minimumSecondsUpDown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown maximumSecondsUpDown;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown desktopSearchesNumericUpDown;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown mobileSearchesNumericUpDown;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ BingRewards Pwner!/MainWindow.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace BingRewardsPwner
+{
+    /// <summary>
+    /// I've created this tool for fun. Yes, could take advantage of Bing Rewards but... Bing is a good search engine, give it a shot!
+    /// </summary>
+    public partial class MainWindow : Form
+    {
+
+        private const string BING_REWARDS_LOGIN_URL = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=12&ct=1430103040&rver=6.0.5286.0&wp=MBI&wreply=https:%2F%2Fwww.bing.com%2Fsecure%2FPassport.aspx%3Frequrl%3Dhttps%253a%252f%252fwww.bing.com%253a443%252frewards%252fdashboard&lc=1033&id=264960";
+
+        /// <summary>
+        /// We want to identify ourselves as a mobile device to perform mobile searches, so we will use this as our User-Agent
+        /// </summary>
+        private const string MOBILE_USER_AGENT = "User-Agent: Mozilla 5.0 (Linux; U; Android 2.3.7; zh-cn; MB525 Build MIUI) UC AppleWebKit 534.31 (KHTML, like Gecko) Mobile Safari 534.31";
+
+        /// <summary>
+        /// Surprisingly enough, there is no easy way to permanently change the User-Agent of the WebBrowser component. The built-in way only last for a request.
+        /// UrlMkSetSessionOption allows us to set the User-Agent for the session. This shouln't affect other instances of IE, but it would be nice to test it.
+        /// More info: https://msdn.microsoft.com/en-us/library/ie/ms775125%28v=vs.85%29.aspx?f=255&MSPPError=-2147217396
+        /// </summary>
+        /// <param name="dwOption">An unsigned long integer value that contains the option to set.</param>
+        /// <param name="pBuffer">A pointer to the buffer containing the new session settings.</param>
+        /// <param name="dwBufferLength">An unsigned long integer value that contains the size of pBuffer.</param>
+        /// <param name="dwReserved">Reserved. Must be set to 0. (I wonder why...)</param>
+        /// <returns></returns>
+        [DllImport("urlmon.dll", CharSet = CharSet.Ansi)]
+        private static extern int UrlMkSetSessionOption(int dwOption, string pBuffer, int dwBufferLength, int dwReserved);
+
+        /// <summary>
+        /// Sets the user agent string for this process.
+        /// </summary>
+        const int URLMON_OPTION_USERAGENT = 0x10000001;
+
+        private bool dashboardFound = false;
+        private bool searchingMobile = false;
+        private string searchTerm;
+        private Timer timer;
+
+        public MainWindow()
+        {
+            timer = new Timer();
+            timer.Tick += Timer_DesktopSearchTick;
+            InitializeComponent();
+            webBrowser1.DocumentCompleted += WebBrowser1_DocumentCompleted;
+        }
+
+        private int CreditTextToSearches(string text)
+        {
+            var chunks = text.Split(new char[] { ' ' });
+            var maximum = Convert.ToInt32(chunks[2]);
+            var current = Convert.ToInt32(chunks.First());
+            var numberOfSearches = (maximum - current) * 2;
+            return numberOfSearches;
+        }
+
+        private void DoSearch()
+        {
+            timer.Interval = new Random(DateTime.Now.Millisecond).Next((int)minimumSecondsUpDown.Value * 1000, (int)maximumSecondsUpDown.Value * 1000);
+            timer.Start();
+        }
+
+        private void Timer_DesktopSearchTick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            webBrowser1.Navigate("http://www.bing.com/search?q=" + searchTerm);
+        }
+
+        private void DealWithWikipedia(WebBrowserDocumentCompletedEventArgs e)
+        {
+            searchTerm = e.Url.ToString().Substring(29).Replace('_', ' ');
+            DoSearch();
+        }
+
+        private void ChangeUserAgent()
+        {
+            UrlMkSetSessionOption(URLMON_OPTION_USERAGENT, MOBILE_USER_AGENT, MOBILE_USER_AGENT.Length, 0);
+        }
+
+        private void DealWithBingResults()
+        {
+            if (!searchingMobile)
+            {
+                desktopSearchesNumericUpDown.Value--;
+                if (desktopSearchesNumericUpDown.Value < 0)
+                {
+                    searchingMobile = true;
+                    ChangeUserAgent();
+                }
+            }
+            else
+                mobileSearchesNumericUpDown.Value--;
+            if (mobileSearchesNumericUpDown.Value > 0 || desktopSearchesNumericUpDown.Value > 0)
+                GetNewSearchTerm();
+            else
+            {
+                startButton.Enabled = true;
+                MessageBox.Show("All searches done. You've achieved all your search points for today.", "Bing Rewards Pwner!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.ToString().StartsWith("http://www.bing.com/search?q="))
+            {
+                DealWithBingResults();
+            }
+            if (e.Url.ToString().StartsWith("http://en.wikipedia.org/wiki/") || e.Url.ToString().StartsWith("http://en.m.wikipedia.org/wiki/"))
+            {
+                DealWithWikipedia(e);
+            }
+            if (e.Url.ToString().StartsWith("https://www.bing.com/msasignin"))
+                FillLoginScreen();
+            if (!dashboardFound)
+            {
+                var mobileSearchInfo = webBrowser1.Document.GetElementById("mobsrch01");
+                if (mobileSearchInfo != null)
+                {
+                    var mobileProgress = ElementsByClass(mobileSearchInfo, "progress");
+                    if (mobileProgress != null)
+                    {
+                        var value = mobileProgress.First().InnerText;
+                        if (value.Contains(" of "))
+                            mobileSearchesNumericUpDown.Value = CreditTextToSearches(value);
+                        else
+                            mobileSearchesNumericUpDown.Value = 0;
+
+                    }
+                }
+                var pcSearchInfo = webBrowser1.Document.GetElementById("srchSMR1216");
+                if (pcSearchInfo != null)
+                {
+                    var pcProgress = ElementsByClass(pcSearchInfo, "progress");
+                    if (pcProgress != null)
+                    {
+                        var value = pcProgress.First().InnerText;
+                        if (value.Contains(" of "))
+                            desktopSearchesNumericUpDown.Value = CreditTextToSearches(value);
+                        else
+                            desktopSearchesNumericUpDown.Value = 0;
+                    }
+                }
+                dashboardFound = desktopSearchesNumericUpDown.Value != -1 && mobileSearchesNumericUpDown.Value != -1;
+                if (dashboardFound)
+                    GetNewSearchTerm();
+            }
+        }
+
+        private void GetNewSearchTerm()
+        {
+            webBrowser1.Navigate("http://en.wikipedia.org/wiki/Special:Random");
+        }
+
+        private void FillLoginScreen()
+        {
+            var user = webBrowser1.Document.GetElementById("i0116");
+            if (user != null)
+                user.SetAttribute("value", textBox1.Text);
+            var password = webBrowser1.Document.GetElementById("i0118");
+            if (password != null)
+                password.SetAttribute("value", textBox2.Text);
+            var button = webBrowser1.Document.GetElementById("idSIButton9");
+            if (button != null && password != null && user != null)
+                button.InvokeMember("Click");
+        }
+
+        /// <summary>
+        /// Retrieves all elements with an specific class name
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="className"></param>
+        /// <returns></returns>
+        private IEnumerable<HtmlElement> ElementsByClass(HtmlElement element, string className)
+        {
+            foreach (HtmlElement e in element.Children)
+                if (e.GetAttribute("className") == className)
+                    yield return e;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(BING_REWARDS_LOGIN_URL);
+            startButton.Enabled = false;
+        }
+    }
+}
